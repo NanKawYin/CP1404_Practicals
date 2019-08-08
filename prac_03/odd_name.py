534177dcbcaa9177e741c6f2de2f7b238d53510d
NanKawYin <kawyin.nan@my.jcu.edu.au>
2019-08-08 10:38:21 +0800

    " Add functions to odd_name"

--- prac_03/odd_name.py
+++ prac_03/odd_name.py
@@ -1,10 +1,23 @@
-"""NanKawYin"""
+def main():
+    """NanKawYin"""
 
-name = str(input("Enter Your Name: "))
-name_count = len(name)
-while name_count == 0:
-    print("User name cannot be empty.")
-    name = input("Enter Your Name: ")
+    name, name_count = get_name()
+    print_name(name, name_count)
+
+
+def print_name(name, name_count):
+    print("Your name is: ", end='')
+    print(name[0:name_count:3])
+
+
+def get_name():
+    name = str(input("Enter Your Name: "))
     name_count = len(name)
-print("Your name is: ",end='')
-print(name[0:name_count+1:3])
+    while name_count == 0:
+        print("User name cannot be empty.")
+        name = input("Enter Your Name: ")
+        name_count = len(name)
+    return name, name_count
+
+
+main()
